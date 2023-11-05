271a9f2f88d28b48033e20f62d9d86c024900959
Guilherme-ags <marcaguild1@gmail.com>
2023-11-05 18:36:53 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,66 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
 
 main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+  var _perguntaSelecionada = 0;
+
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    }
+  ];
 
   void _responder() {
-    setState(() {
-      perguntaSelecionada++;
-    });
-    print(perguntaSelecionada);
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      "Qual é a sua cor favorita?",
-      "Qualé o seu animal favorito?"
-    ];
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [];
+
+    List<Widget> widgetsRespostas =
+        respostas.map((t) => Resposta(t, _responder)).toList();
 
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text("Perguntas"),
       ),
-      body: Column(
-        children: <Widget>[
-          Text(perguntas[perguntaSelecionada]),
-          ElevatedButton(
-            child: Text("Resposta 1"),
-            onPressed: _responder,
-          ),
-          ElevatedButton(
-            child: Text("Resposta 2"),
-            onPressed: _responder,
-          ),
-          ElevatedButton(
-            child: Text("Resposta 3"),
-            onPressed: _responder,
-          ),
-        ],
-      ),
+      body: temPerguntaSelecionada
+          ? Column(
+              children: <Widget>[
+                Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                ...widgetsRespostas,
+              ],
+            )
+          : Center(
+              child: Text(
+                'Parabéns!',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
     ));
   }
 }
